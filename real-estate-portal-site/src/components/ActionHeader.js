3d8132f945bd6006e1368a86c18ccf0f7343f06d
Peter Ekler <evacodekitchen@gmail.com>
2019-09-30 21:47:44 +0200

    remove unnecessary links from navbar

--- real-estate-portal-site/src/components/ActionHeader.js
+++ real-estate-portal-site/src/components/ActionHeader.js
@@ -33,15 +33,7 @@ const ActionHeader = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                    <Nav.Link as={Link} to={"/properties"}>Home</Nav.Link>
                 </Nav>
                 <Form inline onSubmit={(event) => updateSearchedCityInContext(event)}>
                     <FormControl onChange={(event) => updateTypedSearchedCity(event.target.value)} type="text"

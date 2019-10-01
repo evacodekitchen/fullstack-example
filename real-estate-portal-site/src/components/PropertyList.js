43d016685f6f29c90a5933f119fd07d1ea6177cb
Peter Ekler <evacodekitchen@gmail.com>
2019-10-01 12:28:36 +0200

    implement pagination for the properties

--- real-estate-portal-site/src/components/PropertyList.js
+++ real-estate-portal-site/src/components/PropertyList.js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 import {SearchContext} from "../context/SearchContext";
+import Pagination from "react-bootstrap/Pagination"
 import {useContext} from "react"
 import {useEffect} from "react"
 import {useState} from "react"
@@ -16,20 +17,51 @@ const PropertyList = () => {
 
     const [properties, setProperties] = useState([])
 
+    const [activePageIndex, setActivePageIndex] = useState(0)
+
+    const [nrOfPages, setNrOfPages] = useState(0)
+
+    const pageSize = 6
+    const p = [Array(nrOfPages).keys()]
+    const pages = (
+        Array(nrOfPages).fill().map((v, i) => i).map(pageIndex =>
+            <Pagination.Item name="pageIndex" key={pageIndex} active={pageIndex === activePageIndex}
+                             onClick={(event) => {
+                                 turnPage(event)
+                             }}>
+                {pageIndex + 1}
+            </Pagination.Item>
+        )
+    )
+
+
+    const turnPage = (event) => {
+        const pageIndex = event.target.text
+
+        setActivePageIndex(pageIndex - 1)
+    }
+
+
     useEffect(() => {
         let url = "http://localhost:8080/api/v1/properties"
+
+        url = url + `?size=${pageSize}&page=${activePageIndex}`
+
         if (searchedCity) {
-            url = url + "?city=" + searchedCity
+            setActivePageIndex(0)
+            url = url + "&city=" + searchedCity
         }
+
         axios.get(url).then(response => {
-            setProperties(response.data)
+            setNrOfPages(response.data.totalNrOfPages)
+            setProperties(response.data.propertyList)
         }).catch(e => {
             console.log(e)
         })
-    }, [searchedCity])
+    }, [searchedCity, activePageIndex])
 
     const cards = properties.map(property =>
-        <Col key={property.id} className="mt-sm-4">
+        <Col key={property.id} className="mt-4" md={4}>
             <Card style={{width: '18rem'}}>
                 <Card.Img variant="top" src={"data:image/png;base64," + property.picture}/>
                 <Card.Body>
@@ -42,7 +74,17 @@ const PropertyList = () => {
             </Card>
         </Col>
     )
-    return <Container><Row xs={20}>{cards}</Row></Container>
+    return <Container>
+        <Row>{cards}</Row>
+        <Row md={12} className={"mt-4 justify-content-md-center"}>
+
+                <Pagination>
+                    {pages}
+                </Pagination>
+
+
+        </Row>
+    </Container>
 }
 
 export default PropertyList;

c7424d298aaf257d7d0bfc3d49917827d9392056
Eva <evacodekitchen@gmail.com>
2019-10-09 00:29:58 +0200

    spring boot project for Real Estate Portal with  dependencies set

---
+++ RealEstatePortalBackend/src/main/java/edu/ait/realestateportal/RealEstatePortalBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.ait.realestateportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstatePortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstatePortalBackendApplication.class, args);
+	}
+
+}

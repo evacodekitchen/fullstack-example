57099d3d3debb4c9b63f0aa406a81b17b38c579f
Eva <evacodekitchen@gmail.com>
2019-10-12 22:08:08 +0200

    remove unnecessary exact params from the Routes

--- real-estate-portal-site/src/App.js
+++ real-estate-portal-site/src/App.js
@@ -10,14 +10,12 @@ const App = () => {
     return (
         <Router>
             <Switch>
-
                 <Route path="/properties" exact component={SearchRealEstatesPage}/>
-                <Route path="/properties/new" exact component={PropertyCreatorPage}/>
-                <Route path="/properties/:propertyId" exact component={PropertyDetails}/>
+                <Route path="/properties/new" component={PropertyCreatorPage}/>
+                <Route path="/properties/:propertyId" component={PropertyDetails}/>
                 <Route component={NoMatchRoute}/>
             </Switch>
         </Router>
     );
-
 };
 export default App;
